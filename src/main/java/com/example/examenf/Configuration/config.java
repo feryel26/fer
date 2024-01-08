7111ae869d0a0e2f40a56f6c80bc76a06a81353a
feryel26 <feryel.jawedi@esprit.tn>
2024-01-08 09:04:46 +0000

    help222

--- src/main/java/com/example/examenf/Configuration/config.java
+++ src/main/java/com/example/examenf/Configuration/config.java
@@ -27,5 +27,11 @@ public class config {
     //Q3 en utlisant le spring scheduler proposer une methode qui se declanche toute les 15 seconde et qui affiche sur le console la liste des evenements pour chaque categorie
     //void listeEvenementParCategorie();
 
+    //Q4 ajouter les tickets et les affecter aux evenement et aux internaute indique dans le tableau en mettant ajour chaque fois le nombre de place restants dans le table eveneement
+    //NB: si le nombre de ticket a ajouter depasse le nombre de place restant dans un evenement aucun ticket ne peut etre saugarder dans la base de donnees et une execption ser declenchee
+    //List<Ticket> ajouterTicketEtAffecterAEvenementEtInternaute(Liste<Ticket> ticket,Long idEvenement,Long idInternaute);
+
+    //Q5 en ulisant AOP implimenter un aspet qui permet dafficher le message "nb restant depasse" si la methode ajouterTicketEtAffecterAEvenementEtInternaute retoune une exception
+
 
 }
